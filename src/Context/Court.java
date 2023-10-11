8bf94c4d43e28ff21416c3dbb521c5207c8f94df
andreainno22 <andrea.innocenti12@stud.unifi.it>
2023-10-11 15:28:27 +0200

    input bug fixed

--- src/Context/Court.java
+++ src/Context/Court.java
@@ -42,16 +42,14 @@ public class Court {
 
     public void printAllCourt(Formatter fmt, boolean showDiscount) {
         if (this.type.equals("padel"))
-            if(showDiscount)
-                fmt.format("%-15d%-15s%-15.2f%-15.2f\n", id, type, price, price*0.9);
+            if (showDiscount)
+                fmt.format("%-15d%-15s%-15.2f%-15.2f\n", id, type, price, price * 0.9);
             else
-                fmt.format("%-15d%-15s%-15.2f\n", id, type, price*0.9);
-        else
-            if(showDiscount)
-                fmt.format("%-15d%-15s%-15.2f%-15.2f\n", id, terrain_type, price, price*0.9);
-            else
-                fmt.format("%-15d%-15s%-15.2f\n", id, terrain_type, price*0.9);
-        //System.out.println(id + " - " + type + " - " + price+" €");
+                fmt.format("%-15d%-15s%-15.2f\n", id, type, price);
+        else if (showDiscount) {
+            fmt.format("%-15d%-15s%-15.2f%-15.2f\n", id, terrain_type, price, price * 0.9);
+        } else
+            fmt.format("%-15d%-15s%-15.2f\n", id, terrain_type, price);
     }
 
     protected int id;
